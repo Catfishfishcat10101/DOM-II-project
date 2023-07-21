e1a6d3902f03a168d21aebd499cd04ca8b0dd5b4
William <catfishfishcat10101@gmail.com>
2023-07-21 16:14:17 -0500

    3 events added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './less/index.less'
 
-// Your code goes here!
+// LOAD EVENT
+window.onload = function (evt) {
+  console.log('event ${evt.type} fired! REady to go!')
+  const heading = document.querySelector('h1')
+  heading.textContent = 'READY TO GO!!'
+
+  //COPY EVENT
+  window.addEventListener('copy', () => {
+      navigator.clipboard.readText()
+      .then(text => {
+        heading.textContent += text
+      })
+  }) 
+
+  //CLICK EVENT
+  document.body.addEventListener('click', evt => {
+    evt.target.classList.toggle('mirror')  
+  })
+}
+
